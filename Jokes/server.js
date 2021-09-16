91d1c0037dd2b1c7b761fc0f7720e7e23f92207d
michaelalvarez222 <michaelny001@gmail.com>
2021-09-15 22:34:25 -0400

    JokeApiAssignment

--- Jokes/server.js
+++ Jokes/server.js
@@ -8,11 +8,7 @@ app.use( express.urlencoded({ extended: true }) );
 
 require("./server/config/jokes.config")
 
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
-});
-
+require("./server/routes/jokes.routes")(app)
 
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
--- Jokes/server/routes/jokes.routes.js
+++ Jokes/server/routes/jokes.routes.js
@@ -1,3 +1,11 @@
-const JokeController = require("../controllers/joke.controllerb ")
+const JokeController = require("../controllers/joke.controller")
 
-JokeController.
+module.exports = app => {
+    app.get("/api", JokeController.helloworld)
+    app.get("/api/jokes", JokeController.findAllJokes)
+    app.post("/api/jokes/create", JokeController.createNewJoke)
+    app.get("/api/jokes/:id", JokeController.findOneJoke)
+    app.put("/api/jokes/:id", JokeController.updateJoke)
+    app.delete("/api/jokes/:id", JokeController.deleteJoke)
+    app.get("/api/jokes/find/random", JokeController.findRandomJoke)
+}
